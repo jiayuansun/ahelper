f54b029f570fe8cb546a899b6cfafce0ed7995a7
jy <sunjiayuan25@gmail.com>
2018-10-14 11:01:14 -0400

    Iinitla commit

---
+++ mp.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def just_foo_cols(self):
+    """get a list of colum names containing the string 'foo'
+    """
+    return [x for x in self.columns if 'foo' in x]
+
+pd.DataFrame.just_foo_cols=just_foo_cols
+df=pd.DataFrame([list(range(4))],columns=["A","foo","foozball","bar"])
+df.just_foo_cols()
+
+del pd.DataFrame.just_foo_cols
